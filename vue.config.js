8f975a2da74648398199da9c3ac8ec27d30952f0
natalia bolozan <bolozan.natalia@gmail.com>
2024-02-12 20:41:39 +0100

    init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: `/shop/`,
+    publicPath: `/vue-sneakers/`,
     transpileDependencies: true
 })
+//mm
