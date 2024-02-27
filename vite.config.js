66280d59bb6820a4c5352389dde0978d523c3656
natalia bolozan <bolozan.natalia@gmail.com>
2024-02-27 22:47:52 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,30 @@
-// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueJsx(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
-import{defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  base: '/deployng-vue-sneakers',
-  plugins: [vue()]
+ // base: '/vue-sneakers',
+  publicPath: `/vue-sneakers/`,
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
+// import{defineConfig} from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+
+// export default defineConfig({
+//   base: '/deployng-vue-sneakers',
+//   plugins: [vue()]
+// })
