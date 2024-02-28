b91d2ba62901b480ea95304eca670365005fabff
natalia bolozan <bolozan.natalia@gmail.com>
2024-02-28 11:23:19 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//  base: `/vue-sneakers-final`,
-  publicPath: `/vue-sneakers-final/`,
+//  base: `/vue-sneakers`,
+  publicPath: `/vue-sneakers/`,
   plugins: [
     vue(),
     vueJsx(),
